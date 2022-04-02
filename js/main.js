3ea80702439adf0624f52f2bee4dd4444b5b6aaf
SzymonSzmydt <szymonszmydt@gmail.com>
2022-04-02 13:20:35 +0200

    Adding calculator HTML, CSS code + JS file

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("form");
+
+form.addEventListener("DOMEventListener", function (e) {
+    e.preventDefault();
+
+    const listItem = document.querySelectorAll(".list__item");
+    const products = form.querySelector("#product");
+
+    if (products.value === typeof Number) {
+
+    }
+})
+
+
